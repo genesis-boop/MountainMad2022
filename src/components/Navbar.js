a185ad14501780275bef82f62a04f7a00300da75
Alx-Li <li.alex.0901@gmail.com>
2022-04-29 23:20:48 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,25 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className=" bg-green-300 header p-2">
+    <header className=" bg-green-300 header p-3 drop-shadow-md">
       
       <div className=" overflow-x-auto overflow-y-hidden container mx-auto flex justify-between">
         <nav className="my-0 relative sm:container mx-auto flex gap-3 sm:gap-10">
-          <Link href="/">
-            <a className="text-2xl font-bold text-white">About</a>
-          </Link>
-          <Link href="/Recipes">
-            <a className="text-2xl font-bold text-white">Recipes</a>
-          </Link>
-          <Link href="/Ingredients">
-            <a className="text-2xl font-bold text-white">Create</a>
-          </Link>
+          <div className="p-5 bg-blue-500 rounded-lg drop-shadow-md">
+            <Link href="/">
+              <a className="text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 "> About </a>
+            </Link>
+          </div>
+          <div className="p-5 bg-blue-500 rounded-lg drop-shadow-md">
+            <Link href="/Recipes">
+              <a className="text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 ">Recipes</a>
+            </Link>
+          </div>
+          <div className="p-5 bg-blue-500 rounded-lg drop-shadow-md">
+            <Link href="/Ingredients">
+              <a className="text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 ">Create</a>
+            </Link>
+          </div>
         </nav>
       </div>
     </header>
